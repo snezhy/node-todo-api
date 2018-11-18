305ea5bb9467a07408dfbacb3b269a54d0d20ce2
Snezhana <snezhy@rocketmail.com>
2018-11-18 12:41:53 +0000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user'); 
+
+let app = express();
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.listen(3000, () => {
+    console.log("App started on port 3000");
+});
+
+module.exports = { app };
